5f072042a9e4873b31c5d12a456ff40628fef34d
niko <nikolay17538583@gmail.com>
2024-06-20 13:53:13 +0300

    add yandex embedding

---
+++ packages/components/nodes/embeddings/YandexEmbedding/YandexEmbedding.ts
@@ -0,0 +1,81 @@
+import { ICommonObject, INode, INodeData, INodeOptionsValue, INodeParams } from '../../../src/Interface';
+import { getBaseClasses, getCredentialData, getCredentialParam } from '../../../src/utils';
+import { YandexGPTEmbeddings } from '@langchain/yandex';
+
+class YandexEmbedding_Embeddings implements INode {
+    label: string;
+    name: string;
+    version: number;
+    type: string;
+    icon: string;
+    category: string;
+    description: string;
+    baseClasses: string[];
+    credential: INodeParams;
+    inputs: INodeParams[];
+
+    constructor() {
+        this.label = 'Yandex Embeddings';
+        this.name = 'yandexEmbeddings';
+        this.version = 1.0;
+        this.type = 'YandexEmbeddings';
+        this.icon = 'yandex.svg';
+        this.category = 'Embeddings';
+        this.description = 'Yandex API для генерации эмбеддингов';
+        this.baseClasses = [this.type, ...getBaseClasses(YandexGPTEmbeddings)];
+        this.credential = {
+            label: 'Подключить учетные данные',
+            name: 'credential',
+            type: 'credential',
+            credentialNames: ['chatYandexGPT']
+        };
+        this.inputs = [
+            {
+                label: 'Тип модели',
+                name: 'modelType',
+                type: 'options',
+                options: [
+                    { label: 'Document Embedding', name: 'text-search-doc' },
+                    { label: 'Query Embedding', name: 'text-search-query' }
+                ],
+                default: 'text-search-doc'
+            }
+        ];
+    }
+
+    //@ts-ignore
+    loadMethods = {
+        async listModels(): Promise<INodeOptionsValue[]> {
+            return [
+                { label: 'Document Embedding', name: 'text-search-doc' },
+                { label: 'Query Embedding', name: 'text-search-query' }
+            ];
+        }
+    };
+
+    async init(nodeData: INodeData, _: string, options: ICommonObject): Promise<any> {
+        console.log("Инициализация Yandex Embeddings");
+        const modelType = nodeData.inputs?.modelType as string;
+
+        if (nodeData.inputs?.credentialId) {
+            nodeData.credential = nodeData.inputs?.credentialId;
+        }
+        const credentialData = await getCredentialData(nodeData.credential ?? '', options);
+        
+        const yandexApiKey = getCredentialParam('chatYandexGptApiKey', credentialData, nodeData);
+        const folderId = getCredentialParam('chatYandexGptFolderID', credentialData, nodeData);
+
+        const modelUri = `emb://${folderId}/${modelType}/latest`;
+
+        const model = new YandexGPTEmbeddings({
+            apiKey: yandexApiKey,
+            folderID: folderId,
+            modelURI: modelUri
+        });
+
+        console.log("Model URI:", modelUri);
+        return model;
+    }
+}
+
+module.exports = { nodeClass: YandexEmbedding_Embeddings };
